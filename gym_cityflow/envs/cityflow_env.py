e37733219f461e647c9f7b949023aef8ebcdf8b8
Frostyyxo <vandijcksm@gmail.com>
2021-07-26 09:16:08 +1200

    parse roadnet and config json

--- gym_cityflow/envs/cityflow_env.py
+++ gym_cityflow/envs/cityflow_env.py
@@ -1,11 +1,19 @@
+import json
+import cityflow
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-class Cityflow(gym.env):
+class Cityflow(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
+    def __init__(self, configPath):
+        #open cityflow config file into dict
+        self.configDict = json.load(open(configPath))
+        #open cityflow roadnet file into dict
+        self.roadnetDict = json.load(open(self.configDict['dir'] + self.configDict['roadnetFile']))
+        
+        eng = cityflow.Engine(configPath, thread_num=1)
         raise NotImplementedError
 
     def step(self, action):
---
+++ tests/test1.py
@@ -0,0 +1,5 @@
+import gym_cityflow
+import gym
+
+env = gym.make('gym_cityflow:cityflow-v0', 
+               configPath = 'sample_data/sample_config.json')
