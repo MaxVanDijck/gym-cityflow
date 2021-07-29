514571cf40e43e9b2103004c02eb5a6d10f7c60d
Frostyyxo <vandijcksm@gmail.com>
2021-07-29 17:27:51 +1200

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -5,4 +5,4 @@ env = gym.make('gym_cityflow:cityflow-v0',
                configPath = 'sample_data/sample_config.json')
 
 observation = env.step(action=[0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8])
-print(observation)
+#print(observation)
