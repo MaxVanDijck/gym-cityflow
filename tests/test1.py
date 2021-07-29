560263a649bd520efd11033059e62d22d03543f9
Frostyyxo <vandijcksm@gmail.com>
2021-07-29 18:14:34 +1200

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -1,6 +1,7 @@
 import gym_cityflow
 import gym
 import sys
+import os
 
 env = gym.make('gym_cityflow:cityflow-v0', 
                configPath = 'sample_data/sample_config.json')
@@ -8,12 +9,9 @@ env = gym.make('gym_cityflow:cityflow-v0',
 testAction = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8]
 
 #disable print temporarily
-sys.stdout = open(os.devnull, 'w')
 #iterate environment a lttle bit to test env
 for i in range(1000):
     observation, reward = env.step(action=testAction)
 
-#enable print and step
-sys.stdout = sys.__stdout__
 observation, reward = env.step(action=testAction)
 #print(observation)
