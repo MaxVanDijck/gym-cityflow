089553eea0ca15d097f5cef727c9082cfef0f19a
Frostyyxo <vandijcksm@gmail.com>
2021-08-03 17:07:05 +1200

    randomly generate traffic phases

--- tests/test1.py
+++ tests/test1.py
@@ -2,15 +2,22 @@ import gym_cityflow
 import gym
 import sys
 import os
+import random
 
 env = gym.make('gym_cityflow:cityflow-v0', 
                configPath = 'sample_data/sample_config.json')
 
-testAction = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8]
-
+testAction = []
+for i in range(0,16):
+    n = random.randint(0,8)
+    testAction.append(n)
 #disable print temporarily
 #iterate environment a lttle bit to test env
 for i in range(1000):
+    testAction = []
+    for i in range(0,16):
+        n = random.randint(0,8)
+        testAction.append(n)
     observation, reward = env.step(action=testAction)
 
 observation, reward = env.step(action=testAction)
