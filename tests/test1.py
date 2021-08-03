cc641d2c1f484d29677eedaa6e2d3af3e9339b33
Frostyyxo <vandijcksm@gmail.com>
2021-08-03 18:03:41 +1200

    action interval

--- tests/test1.py
+++ tests/test1.py
@@ -13,11 +13,14 @@ for i in range(0,16):
     testAction.append(n)
 #disable print temporarily
 #iterate environment a lttle bit to test env
+actionInterval = 10
+
 for i in range(1000):
-    testAction = []
-    for i in range(0,16):
-        n = random.randint(0,8)
-        testAction.append(n)
+    if i % actionInterval == 0:
+        testAction = []
+        for i in range(0,16):
+            n = random.randint(0,8)
+            testAction.append(n)
     observation, reward = env.step(action=testAction)
 
 observation, reward = env.step(action=testAction)
