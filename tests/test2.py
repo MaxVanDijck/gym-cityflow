f36586e879b7ff023d8e744a9448e30e9d397ecf
Frostyyxo <vandijcksm@gmail.com>
2021-08-05 14:51:55 +1200

    second test file

---
+++ tests/test2.py
@@ -0,0 +1,34 @@
+import gym_cityflow
+import gym
+import sys
+import os
+import random
+
+env = gym.make('gym_cityflow:cityflow-v0', 
+               configPath = 'sample_data/sample_config_2.json',
+               episodeSteps = 1000)
+
+#Check action space
+print(env.action_space)
+print(env.observation_space)
+
+#disable print temporarily
+#iterate environment a lttle bit to test env
+actionInterval = 10
+
+for i in range(10000):
+    if i % actionInterval == 0:
+        testAction = []
+        for i in range(0,1):
+            n = random.randint(0,8)
+            testAction.append(n)
+    observation, reward, done, debug = env.step(action=testAction)
+    if done:
+        break
+
+observation, reward, done, debug = env.step(action=testAction)
+print(observation)
+print(reward)
+
+observation = env.reset()
+print(observation)
