ad1c0ae56b951f481614d89d71d661fe241c820b
Max van Dijck <vandijcksm@gmail.com>
2022-04-13 09:09:52 +1200

    fix gym.make calls in Readme and tests

--- tests/test1.py
+++ tests/test1.py
@@ -4,23 +4,27 @@ import sys
 import os
 import random
 
-env = gym.make('gym_cityflow:cityflow-v0', 
-               configPath = 'sample_data/sample_config.json',
-               episodeSteps = 3600)
+env = gym.make(
+    "cityflow-v0",
+    configPath="sample_data/sample_config.json",
+    episodeSteps=3600,
+)
 
-#Check action space
+# Check action space
 print(env.action_space)
 print(env.observation_space)
+env.reset()
 
-#disable print temporarily
-#iterate environment a lttle bit to test env
+# disable print temporarily
+# iterate environment a lttle bit to test env
 actionInterval = 10
 
-for i in range(10000):
+
+for i in range(10):
     if i % actionInterval == 0:
         testAction = []
-        for i in range(0,16):
-            n = random.randint(0,8)
+        for i in range(0, 16):
+            n = random.randint(0, 8)
             testAction.append(n)
     observation, reward, done, debug = env.step(action=testAction)
     if done:
--- tests/test2.py
+++ tests/test2.py
@@ -4,23 +4,24 @@ import sys
 import os
 import random
 
-env = gym.make('gym_cityflow:cityflow-v0', 
-               configPath = 'sample_data/sample_config_2.json',
-               episodeSteps = 3600)
+env = gym.make(
+    "cityflow-v0", configPath="sample_data/sample_config_2.json", episodeSteps=3600
+)
 
-#Check action space
+# Check action space
 print(env.action_space)
 print(env.observation_space)
+env.reset()
 
-#disable print temporarily
-#iterate environment a lttle bit to test env
+# disable print temporarily
+# iterate environment a lttle bit to test env
 actionInterval = 10
 
-for i in range(10000):
+for i in range(10):
     if i % actionInterval == 0:
         testAction = []
-        for i in range(0,1):
-            n = random.randint(0,8)
+        for i in range(0, 1):
+            n = random.randint(0, 8)
             testAction.append(n)
     observation, reward, done, debug = env.step(action=testAction)
     if done:
